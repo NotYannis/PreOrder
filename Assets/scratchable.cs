9f609790d80df77969b8301102633d87ac797c8f
Yannis Attard <yannis.attard@gmail.com>
2016-10-17 14:39:34 +0200

    sliding with the mouse works, need to test with tactile

--- Assets/scratchable.cs
+++ Assets/scratchable.cs
@@ -22,6 +22,12 @@ public class scratchable : MonoBehaviour
     int lastPosX = 0;
     int lastPosY = 0;
     
+	Touch inputTouch = new Touch();
+
+
+    Vector3 lastMousePos;
+    Vector3 lastTouchPos;
+
     void Start()
     {
         Texture2D texture = new Texture2D(1750, 800);
@@ -37,98 +43,176 @@ public class scratchable : MonoBehaviour
             }
         }
         texture.Apply();
+        lastMousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
     }
 
     void Update()
-    {
-        /*
-        * Mobile input 
-        */
-        int nTouch = 0;
-        Touch inputTouch = new Touch();
-        foreach (Touch touch in Input.touches)
-        {
-            if (touch.phase == TouchPhase.Began)
-            {
+	{
+		mouseInput();
+        lastMousePos = Camera.main.WorldToScreenPoint(Input.mousePosition);
+        lastTouchPos = Camera.main.WorldToScreenPoint(inputTouch.position);
 
-            }
-            inputTouch = Input.touches[0];
-            nTouch++;
-        }
+    }
 
-        /*
+	/*
+    * Draw circle on player input 
+    */
+	void mouseInput(){
+		/*
+		if(Input.GetMouseButton(0))
+		{
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+			if (Physics.Raycast(ray))
+			{
+				Texture2D texture = GetComponent<Renderer>().material.mainTexture as Texture2D;
+				Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+				int posX = Mathf.RoundToInt(pos.x);
+				int posY = Mathf.RoundToInt(pos.y);
+
+				// Draw a circle with the mouse position in center
+				// We repeat the algorithm with a lesser radius each time
+
+				int tempRadius = radius;
+				while(tempRadius >= 0)
+				{
+					// Andres circle algorithm
+					int x = 0;
+					int y = tempRadius;
+					int d = tempRadius - 1;
+
+					while (y >= x)
+					{
+						texture.SetPixel(posX + x, posY + y, Color.clear);
+						texture.SetPixel(posX + y, posY + x, Color.clear);
+						texture.SetPixel(posX - x, posY + y, Color.clear);
+						texture.SetPixel(posX - y, posY + x, Color.clear);
+						texture.SetPixel(posX + x, posY - y, Color.clear);
+						texture.SetPixel(posX + y, posY - x, Color.clear);
+						texture.SetPixel(posX - x, posY - y, Color.clear);
+						texture.SetPixel(posX - y, posY - x, Color.clear);
+
+						if (d >= 2 * x)
+						{
+							d -= 2 * x + 1;
+							x++;
+						}
+						else if (d < 2 * (tempRadius - y))
+						{
+							d += 2 * y - 1;
+							y--;
+						}
+						else
+						{
+							d += 2 * (y - x - 1);
+							y--;
+							x++;
+						}
+					}
+
+					tempRadius--;
+				}
+
+				texture.Apply();
+				GetComponent<Renderer>().material.mainTexture = texture;
+			}
+		}*/
+
+		if(Input.GetMouseButton (0)) {
+			Vector3 mousePos = Camera.main.WorldToScreenPoint(Input.mousePosition);
+
+			Vector3 move = mousePos - lastMousePos;
+			Camera.main.transform.position += new Vector3(move.x, move.y, 0.0f);
+		}
+	}
+
+	/*
+    * Mobile input 
+    */
+	void touchInput(){
+		int nTouch = 0;
+
+		foreach (Touch touch in Input.touches)
+		{
+			if (touch.phase == TouchPhase.Began)
+			{
+
+			}
+			inputTouch = Input.touches[0];
+			nTouch++;
+		}
+
+
+		/*
          * Move the screen when there is more than one touch
         */
-        if(nTouch > 1)
-        {
-            // inputTouch.position;
-        }
-        
-        /*
-         * Draw circle on player input 
-        */
-        if(Input.GetMouseButton(0))
-        // if (nTouch == 1)
-        {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            // Ray ray = Camera.main.ScreenPointToRay(inputTouch.position);
+		if(nTouch > 1)
+		{
+			Vector3 touchPos = Camera.main.WorldToScreenPoint(inputTouch.position);
 
-            if (Physics.Raycast(ray))
-            {
-                Texture2D texture = GetComponent<Renderer>().material.mainTexture as Texture2D;
-                Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-                // Vector3 pos = Camera.main.ScreenToWorldPoint(inputTouch.position);
+			Vector3 move = touchPos - lastTouchPos;
+			Camera.main.transform.position += new Vector3(move.x, move.y, 0.0f);		}
 
-                int posX = Mathf.RoundToInt(pos.x);
-                int posY = Mathf.RoundToInt(pos.y);
-                
+		if (nTouch == 1)
+		{
+			Ray ray = Camera.main.ScreenPointToRay(inputTouch.position);
 
-               // Draw a circle with the mouse position in center
-               // We repeat the algorithm with a lesser radius each time
-               
-               int tempRadius = radius;
-               while(tempRadius >= 0)
-               {
-                   // Andres circle algorithm
-                   int x = 0;
-                   int y = tempRadius;
-                   int d = tempRadius - 1;
+			if (Physics.Raycast(ray))
+			{
+				Texture2D texture = GetComponent<Renderer>().material.mainTexture as Texture2D;
+				Vector3 pos = Camera.main.ScreenToWorldPoint(inputTouch.position);
 
-                   while (y >= x)
-                   {
-                       texture.SetPixel(posX + x, posY + y, Color.clear);
-                       texture.SetPixel(posX + y, posY + x, Color.clear);
-                       texture.SetPixel(posX - x, posY + y, Color.clear);
-                       texture.SetPixel(posX - y, posY + x, Color.clear);
-                       texture.SetPixel(posX + x, posY - y, Color.clear);
-                       texture.SetPixel(posX + y, posY - x, Color.clear);
-                       texture.SetPixel(posX - x, posY - y, Color.clear);
-                       texture.SetPixel(posX - y, posY - x, Color.clear);
+				int posX = Mathf.RoundToInt(pos.x);
+				int posY = Mathf.RoundToInt(pos.y);
 
-                       if (d >= 2 * x)
-                       {
-                           d -= 2 * x + 1;
-                           x++;
-                       }
-                       else if (d < 2 * (tempRadius - y))
-                       {
-                           d += 2 * y - 1;
-                           y--;
-                       }
-                       else
-                       {
-                           d += 2 * (y - x - 1);
-                           y--;
-                           x++;
-                       }
-                   }
 
-                   tempRadius--;
-               }
-               
-                texture.Apply();
-                GetComponent<Renderer>().material.mainTexture = texture;
-            }
-        }
-    }
+				// Draw a circle with the mouse position in center
+				// We repeat the algorithm with a lesser radius each time
+
+				int tempRadius = radius;
+				while(tempRadius >= 0)
+				{
+					// Andres circle algorithm
+					int x = 0;
+					int y = tempRadius;
+					int d = tempRadius - 1;
+
+					while (y >= x)
+					{
+						texture.SetPixel(posX + x, posY + y, Color.clear);
+						texture.SetPixel(posX + y, posY + x, Color.clear);
+						texture.SetPixel(posX - x, posY + y, Color.clear);
+						texture.SetPixel(posX - y, posY + x, Color.clear);
+						texture.SetPixel(posX + x, posY - y, Color.clear);
+						texture.SetPixel(posX + y, posY - x, Color.clear);
+						texture.SetPixel(posX - x, posY - y, Color.clear);
+						texture.SetPixel(posX - y, posY - x, Color.clear);
+
+						if (d >= 2 * x)
+						{
+							d -= 2 * x + 1;
+							x++;
+						}
+						else if (d < 2 * (tempRadius - y))
+						{
+							d += 2 * y - 1;
+							y--;
+						}
+						else
+						{
+							d += 2 * (y - x - 1);
+							y--;
+							x++;
+						}
+					}
+
+					tempRadius--;
+				}
+
+				texture.Apply();
+				GetComponent<Renderer>().material.mainTexture = texture;
+			}
+		}
+	}
 }
